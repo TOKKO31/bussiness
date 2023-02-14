f589842d710c3fdbe3adf5f2a17960b5f25fcdaa
TOKKO31 <117857539+TOKKO31@users.noreply.github.com>
2023-02-14 20:23:54 +0400

    finish

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
+// JavaScript to toggle the menu and make it fixed
 var toggleButton = document.querySelector(".toggle-menu");
 var navBar = document.querySelector(".nav-bar");
-toggleButton.addEventListener("click", function () {
+var closeButton = document.querySelector(".close-menu");
+
+// function to toggle the menu when the button is clicked
+function toggleMenu() {
+  if (navBar.classList.contains("fixed")) {
+    navBar.classList.remove("fixed");
+  } else {
+    navBar.classList.add("fixed");
+  }
   navBar.classList.toggle("toggle");
+}
+
+// Event listener for the toggle button
+toggleButton.addEventListener("click", toggleMenu);
+
+//Event listener for the close button
+closeButton.addEventListener("click", function () {
+  navBar.classList.remove("fixed");
+  navBar.classList.remove("toggle");
 });
